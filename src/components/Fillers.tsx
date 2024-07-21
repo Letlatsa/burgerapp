8a4f30e35b497eb3cd749a9677a5ca68a6587e3e
Letlatsa <leboletlatsa@gmail.com>
2024-07-21 11:30:48 +0200

    Drop down menu for the fillers

--- src/components/Fillers.tsx
+++ src/components/Fillers.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-
 interface Props {
-FillerType:string;
-FillerColor:string;
+  FillerType: string;
+  FillerColor: string;
 }
 
-const Fillers :React.FC<Props> = ({FillerColor , FillerType})=> {
-
-    return(<div style={{backgroundColor :FillerColor }}>{FillerType}</div>);
+const Fillers: React.FC<Props> = ({ FillerColor, FillerType }) => {
+  return <div style={{ backgroundColor: FillerColor }}>{FillerType}</div>;
 };
 
 export default Fillers;
